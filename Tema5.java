dadd98cc518e935878549c986f1980aab6289d35
marianneaga <85581867+marianneaga@users.noreply.github.com>
2021-06-29 18:58:45 +0300

    Add files via upload

---
+++ Tema5.java
@@ -0,0 +1,13 @@
+package Homeworks;
+
+public class Tema5 {
+    public static void main(String[] args) {
+        String[] s1 = {"Marian" , "are" , "gogosi"};
+        String phrase= "" ;
+        for (int i=0; i<s1.length; i++){
+            phrase=phrase + s1[i] + " ";
+        }
+        System.out.println(phrase);
+    }
+
+}
